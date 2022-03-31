ac61c54553d0196311817483a6faaf4d953f324f
zhj <1162578140@qq.com>
2022-03-31 12:35:30 +0800

    项目初始化-引入vant

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <!-- 路由出口 -->
     <router-view/>
     <h1>黑马头条</h1>
+    <i class="toutiao toutiao-shouye"></i>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 引入vant组件库
+import Vant from 'vant'
+// 引入vant组件库样式
+import 'vant/lib/index.css'
+// 注册并使用vant组件
+Vue.use(Vant)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
